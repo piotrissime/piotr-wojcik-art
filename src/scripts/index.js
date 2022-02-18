66d3e78f32758df686ccf142859caa3eba6bc175
piotrissime <lepetitmothais@gmail.com>
2022-02-18 17:04:49 +0100

    first commit

---
+++ src/scripts/index.js
@@ -0,0 +1,23 @@
+// IMPORT FUNCTIONS
+
+// Home page
+// import { initHome } from './pages/home.js';
+
+// Page 1
+// import initPage1 from './pages/page1.js';
+
+// CALL FUNCTIONS
+/*
+const main = async () => {
+    switch (window.location.pathname.split('/').pop()) {
+        case '':
+        case 'index.html':
+            await initHome();
+            break;
+        case 'page1.html':
+            await initPage1();
+            break;
+    }
+};
+
+main(); */
