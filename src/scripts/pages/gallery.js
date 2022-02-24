2e2ae21c11b6c95b6b2c082fd1132ce3417d0d9c
Léonard Wojcik <leonardw@tutanota.com>
2022-02-24 11:58:46 +0100

    feat: add filter options to gallery

--- src/scripts/pages/gallery.js
+++ src/scripts/pages/gallery.js
@@ -1,7 +1,13 @@
-// const galleryItem = document.querySelectorAll('.gallery__item');
+// TODO : fix currentIndex bug after filtering medias
+
+// Medias
+const medias = document.querySelectorAll('.gallery__item img');
+let displayedMedias = document.querySelectorAll('.gallery__item img');
+// Filter buttons
+const filterButtons = document.querySelectorAll('.filter-button');
+// Lightbox
 const lightbox = document.querySelector('.lightbox');
 const closeLightboxButton = document.querySelector('.lightbox__close-button');
-const medias = document.querySelectorAll('.gallery__item img');
 const lightboxCurrentImg = document.querySelector('.lightbox__current-img');
 const previousButton = document.querySelector('.lightbox__previous-image');
 const nextButton = document.querySelector('.lightbox__next-image');
@@ -17,7 +23,7 @@ const closeLightbox = () => {
 // Open and close lightbox
 const enableLightbox = () => {
     // Open lightbox
-    medias.forEach((element, index) => {
+    displayedMedias.forEach((element, index) => {
         element.addEventListener('click', () => {
             let mediaSrc = element.src;
             lightboxCurrentImg.setAttribute('src', mediaSrc);
@@ -34,7 +40,7 @@ const enableLightbox = () => {
 // Set src to current media
 const setMediaSrc = () => {
     const currentMedia = document.querySelector('.lightbox__current-img');
-    currentMedia.src = medias[currentIndex].src;
+    currentMedia.src = displayedMedias[currentIndex].src;
 };
 
 // Next and previous media inside lightbox
@@ -42,7 +48,7 @@ const lightboxControls = () => {
     const previousMedia = () => {
         // If at the beginning of the array, go to the end of the array
         if (currentIndex === 0) {
-            currentIndex = medias.length - 1;
+            currentIndex = displayedMedias.length - 1;
             setMediaSrc();
         } else {
             currentIndex--;
@@ -52,7 +58,7 @@ const lightboxControls = () => {
 
     const nextMedia = () => {
         // If at the end of the array, go to the beginning of the array
-        if (currentIndex === medias.length - 1) {
+        if (currentIndex === displayedMedias.length - 1) {
             currentIndex = 0;
             setMediaSrc();
         } else {
@@ -66,7 +72,59 @@ const lightboxControls = () => {
     nextButton.addEventListener('click', nextMedia);
 };
 
+const filterMedias = () => {
+    const filterMedias = (category) => {
+        if (category !== 'Tous') {
+            let filteredElements = document.querySelectorAll(
+                `.gallery__item img:not(.${category})`
+            );
+            for (const iterator of filteredElements) {
+                iterator.parentNode.style.display = 'none';
+            }
+            displayedMedias = document.querySelectorAll(
+                `.gallery__item img.${category}`
+            );
+            // Display filtered media if previously filtered
+            for (const iterator of displayedMedias) {
+                iterator.parentNode.style.display = 'block';
+            }
+            console.log(displayedMedias);
+        } else {
+            for (const iterator of medias) {
+                iterator.parentNode.style.display = 'block';
+            }
+            displayedMedias = medias;
+        }
+    };
+
+    for (const iterator of filterButtons) {
+        iterator.addEventListener('click', (e) => {
+            switch (e.target.value) {
+                case 'Peintures noir et blanc':
+                    filterMedias('black-white');
+                    break;
+
+                case 'Peintures couleur':
+                    filterMedias('colour');
+                    break;
+
+                case 'Dessins':
+                    filterMedias('drawing');
+                    break;
+
+                case 'Tous':
+                    filterMedias('Tous');
+                    break;
+
+                default:
+                    break;
+            }
+        });
+    }
+};
+
 export const initGallery = () => {
     enableLightbox();
     lightboxControls();
+    filterMedias();
 };
