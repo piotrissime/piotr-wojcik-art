18fb1b47c4cdb7de03509b918808d0e6104a343b
Léonard Wojcik <leonardw@tutanota.com>
2023-09-03 11:08:50 +0200

    feat: add scroll to top button in gallery page

--- src/scripts/gallery.js
+++ src/scripts/gallery.js
@@ -21,6 +21,9 @@ const closeLightbox = () => {
     lightbox.style.display = 'none';
 };
 
+// Scroll to top button
+const scrollToTopButton = document.querySelector(".scroll-to-top");
+
 // Open and close lightbox
 const enableLightbox = () => {
     // Open lightbox
@@ -162,10 +165,30 @@ const filterMedias = () => {
     }
 };
 
+const enableScrollToTop = () => {
+
+    // Display the "scroll to top button" according to scroll position.
+    window.addEventListener("scroll", () => {
+  if (window.pageYOffset > 100) {
+    scrollToTopButton.classList.add("scroll-to-top_active");
+  } else {
+    scrollToTopButton.classList.remove("scroll-to-top_active");
+  }
+})
+
+    // Add "scroll to top" feature to button
+    scrollToTopButton.addEventListener('click', () => {
+        window.scrollTo({
+            top: 0
+        })
+    })
+}
+
 export const initGallery = () => {
     enableLightbox();
     lightboxControls();
     filterMedias();
+    enableScrollToTop();
 };
 
 initGallery();
