9e996f0b3662621b9943ed780d9426c6bb0e958d
Léonard Wojcik <leonardw@tutanota.com>
2025-12-16 20:40:14 +0100

    fix: filtering

--- src/components/GalleryComponent.tsx
+++ src/components/GalleryComponent.tsx
@@ -2,6 +2,8 @@ import "photoswipe/dist/photoswipe.css";
 import classes from "./GalleryComponent.module.css";
 import { Gallery } from "react-photoswipe-gallery";
 import GalleryItem from "./GalleryItem";
+import Filters from "./Filters";
+import { useState } from "react";
 
 // TODO: bug found -> Filter to "colors" and swap some images ; some black and white images will show.
 
@@ -12,28 +14,38 @@ interface GalleryProps {
 export default function GalleryComponent({ images }: GalleryProps) {
   // Reverse the order of the images (from latests to oldest)
   const reversedImages = [...images].reverse();
+  const [filter, setFilter] = useState("tous");
+
+  const filteredImages =
+    filter === "tous"
+      ? reversedImages
+      : reversedImages.filter((image) => image.frontmatter.type === filter);
+
   return (
-    <div className={classes.gallery}>
-      <Gallery>
-        {reversedImages.map((image: any, index: number) => (
-          <GalleryItem
-            key={index}
-            index={index}
-            optimizedImagesThumbnail={
-              image.frontmatter.optimizedImagesThumbnail
-            }
-            optimizedImagesFull={image.frontmatter.optimizedImagesFull}
-            type={image.frontmatter.type}
-            alt={image.frontmatter.alt}
-            artWidth={image.frontmatter.artWidth}
-            artHeight={image.frontmatter.artHeight}
-            unit={image.frontmatter.unit}
-            originalWidth={image.frontmatter.originalWidth}
-            originalHeight={image.frontmatter.originalHeight}
-            price={image.frontmatter.price}
-          />
-        ))}
-      </Gallery>
-    </div>
+    <>
+      <Filters setFilter={setFilter} filter={filter} />
+      <div className={classes.gallery}>
+        <Gallery>
+          {filteredImages.map((image: any, index: number) => (
+            <GalleryItem
+              key={index}
+              index={index}
+              optimizedImagesThumbnail={
+                image.frontmatter.optimizedImagesThumbnail
+              }
+              optimizedImagesFull={image.frontmatter.optimizedImagesFull}
+              type={image.frontmatter.type}
+              alt={image.frontmatter.alt}
+              artWidth={image.frontmatter.artWidth}
+              artHeight={image.frontmatter.artHeight}
+              unit={image.frontmatter.unit}
+              originalWidth={image.frontmatter.originalWidth}
+              originalHeight={image.frontmatter.originalHeight}
+              price={image.frontmatter.price}
+            />
+          ))}
+        </Gallery>
+      </div>
+    </>
   );
 }
